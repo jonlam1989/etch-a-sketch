84b67601f71f91b6b4e3892564a9985e39f7201d
jonlam1989 <jonlam1989@outlook.com>
2022-01-01 14:57:05 -0500

    added html-grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,25 @@
 let grid = document.querySelector('.s2-grid')
 let buttons = document.querySelectorAll('button')
 
+const gridWidth = grid.clientWidth
+const gridHeight = grid.clientHeight
+
+let row = 16
+let col = 16
+let cellWidth = gridWidth / col
+let cellHeight = gridHeight / row
+
+for (let i = 0; i < row; i++) {
+    let gridRow = document.createElement('tr')
+    for (let j = 0; j < col; j++) {
+        let gridCell = document.createElement('td')
+        gridCell.style.width = `${cellWidth}px`
+        gridCell.style.height = `${cellHeight}px`    
+        gridRow.append(gridCell)
+    }
+    grid.append(gridRow)
+}
+
 for (let btn of buttons) {
     btn.addEventListener('click', ()=>{
         console.log('clicked')
